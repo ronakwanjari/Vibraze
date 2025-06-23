32251054c66f38990dec965511cf19ce3db23271
ronakwanjari <ronakwanjari22@gmail.com>
2025-06-24 01:50:55 +0530

    done

--- script.js
+++ script.js
@@ -1,3 +1,64 @@
+function locomotiveAnimation() {
+    gsap.registerPlugin(ScrollTrigger);
+
+    const locoScroll = new LocomotiveScroll({
+        el: document.querySelector("#main"),
+        smooth: true,
+
+        // for tablet smooth
+        tablet: { smooth: true },
+
+        // for mobile
+        smartphone: { smooth: true }
+    });
+    locoScroll.on("scroll", ScrollTrigger.update);
+
+    ScrollTrigger.scrollerProxy("#main", {
+        scrollTop(value) {
+            return arguments.length
+                ? locoScroll.scrollTo(value, 0, 0)
+                : locoScroll.scroll.instance.scroll.y;
+        },
+        getBoundingClientRect() {
+            return {
+                top: 0,
+                left: 0,
+                width: window.innerWidth,
+                height: window.innerHeight
+            };
+        }
+    });
+
+    ScrollTrigger.addEventListener("refresh", () => locoScroll.update());
+
+    ScrollTrigger.refresh();
+}
+
+function loadingAnimation() {
+
+    var tl = gsap.timeline()
+    tl.from("#page1", {
+        opacity: 0,
+        duration: 0.2,
+        delay: 0.2
+    })
+    tl.from("#page1", {
+        transform: "scaleX(0.7) scaleY(0.2) translateY(80%)",
+        borderRadius: "150px",
+        duration: 2,
+        ease: "expo.out"
+    })
+    tl.from("nav", {
+        opacity: 0,
+        delay: -0.2
+    })
+    tl.from("#page1 h1, #page1 p, #page1 div", {
+        opacity: 0,
+        duration: 0.5,
+        stagger: 0.2
+    })
+}
+
 function navanimation() {
     var nav = document.querySelector("nav")
 
@@ -36,7 +97,6 @@ nav.addEventListener("mouseleave",function(){
      })
 })
 }
-navanimation()
 
 function page2Animation() {
     var rightElems = document.querySelectorAll(".right-elem")
@@ -63,7 +123,6 @@ function page2Animation() {
         })
     })
 }
-page2Animation()
 
 function page3VideoAnimation() {
     var page3Center = document.querySelector(".page3-center")
@@ -101,5 +160,25 @@ function page3VideoAnimation() {
     })
 
 }
- page3VideoAnimation()
- 
+
+function page6Animations() {
+    gsap.from("#btm6-part2 h4", {
+        x: 0,
+        duration: 1,
+        scrollTrigger: {
+            trigger: "#btm6-part2",
+            scroller: "#main",
+            start: "top 80%",
+            end: "top 10%",
+            scrub: true
+        }
+    })
+}
+
+
+locomotiveAnimation() 
+navanimation()
+page2Animation()
+page3VideoAnimation()
+page6Animations()
+loadingAnimation()
